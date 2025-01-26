2959e50e68804c640175f7380d6b3b235594cb71
Isaque Lins <isaquelins523@gmail.com>
2025-01-25 23:19:30 -0300

    Correção da main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
 import { ToastContainer } from 'react-toastify';
-=======
->>>>>>> c85ab364556b9c24dfe3f233e0231118c7da96df
 
 import { Login } from './containers/Login';
 import GlobalStyles from './styles/globalStyles';
