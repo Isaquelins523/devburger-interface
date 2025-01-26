c85ab364556b9c24dfe3f233e0231118c7da96df
Isaque Lins <isaquelins523@gmail.com>
2025-01-25 22:52:27 -0300

    Aplicando responsividade

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import GlobalStyles from './styles/globalStyles';
 
 import { Login } from './containers/Login';
+import GlobalStyles from './styles/globalStyles';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
